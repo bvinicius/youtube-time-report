95ae4ec9a25d2a96624a38a46194bf5ad4570150
bvinicius <vinicius.bazanella1@gmail.com>
2023-04-08 23:59:05 +0100

    Removes temporarily WindowAndTab actions observer

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,14 +1,11 @@
 import { RemoteVideoCounterMessage } from './secondary/RemoteVideoCounterMessage';
 import StatisticsStorageServiceImpl from './secondary/StatisticsStorageServiceImpl';
-import WindowAndTabActionsObserver from './secondary/WindowAndTabActionsObserver';
 import VideoTimeCounter from './secondary/VideoTimeCounter';
 
 const statisticsService = new StatisticsStorageServiceImpl();
 const videoCounter = new VideoTimeCounter(statisticsService);
 
 chrome.runtime.onMessage.addListener((message: RemoteVideoCounterMessage) => {
-	initializeTabObserver(message.payload.videoId);
-
 	switch (message.type) {
 		case 'enableCounter':
 			onEnableCounter(message.payload.videoId);
@@ -18,18 +15,6 @@ chrome.runtime.onMessage.addListener((message: RemoteVideoCounterMessage) => {
 			break;
 	}
 });
-
-function initializeTabObserver(videoId: string) {
-	const tabActionObserver = new WindowAndTabActionsObserver({
-		onTabFocus: onEnableCounter,
-		onTabBlur: onDisableCounter,
-		onWindowFocus: onEnableCounter,
-		onWindowBlur: onDisableCounter,
-		tabFilterCallback: (tab) => tab.url?.includes(videoId) || false,
-	});
-
-	tabActionObserver.observe();
-}
 
 function onEnableCounter(videoId?: string) {
 	videoCounter.enableCounter(videoId);
