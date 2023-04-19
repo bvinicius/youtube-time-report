4ec48188b183dd69978c800f38977d3137497128
bvinicius <vinicius.bazanella1@gmail.com>
2023-04-19 08:56:32 +0100

    Adds unit tests for date-utils

---
+++ src/background/test/date-utils.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect, describe } from 'vitest';
+import { isoDate, subtractDays } from '../src/infrastructure/utils/date-utils';
+
+describe('iso date', () => {
+	test('should get the ISO date from a normal Date object.', () => {
+		const date = new Date('2021-01-01');
+		const result = isoDate(date);
+		expect(result).toBe('2021-01-01');
+	});
+});
+
+describe('subtract days', () => {
+	test('should subtract days from a date', () => {
+		const date = new Date('2021-01-01');
+		const result = subtractDays(date, 1);
+		expect(result.toString()).equals(new Date('2020-12-31').toString());
+	});
+});
